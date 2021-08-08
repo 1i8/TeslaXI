f3a6958d29351cb2497a4ab8f202045e585750ea
TheLeftExit <theleftexit@protonmail.com>
2021-08-08 19:32:46 +0300

    Working object model & restructuring

--- TheLeftExit.Growtopia/Decoding/ItemsDAT.cs
+++ TheLeftExit.Growtopia/Decoding/ItemsDAT.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheLeftExit.Growtopia
+namespace TheLeftExit.Growtopia.Decoding
 {
     public static class ItemsDAT
     {
